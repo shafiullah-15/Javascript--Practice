ad769c4e64b27794576fe61ce8cf0edeb56bd082
SHAFI ULLAH <shafiullah15official@gmail.com>
2025-05-12 09:49:00 +0500

    my message

---
+++ Task 1/javascript.js
@@ -0,0 +1,79 @@
+//  let pragraph="On the Insert tab,the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks. When you create pictures, charts, or diagrams, they also coordinate with your current document look.You can easily change the formatting of selected text in the document text by choosing a look for the selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly.To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template."
+// let userSearch=prompt("Search To Any Word")
+// let counter=0
+// for(let i = 0 ; i < pragraph.length ; i++){
+  
+//     if(userSearch==pragraph.slice(i,i+userSearch.length) ){
+//         ++counter
+//         console.log(alert(userSearch+" This Word Is Match"))
+//         break
+//     }
+//     else{
+//         console.log("No Match")
+       
+
+//     }
+
+
+// // }
+
+// let studentName=["Shafi","Ramzan","Arif","Asif","Noor"]
+// let upperName=[]
+
+// for (let i=0; i<studentName.length; i++){
+// // console.log(studentName[i].toUpperCase())
+// upperName.unshift(studentName[i].to())
+
+// }
+//  console.log(upperName);
+
+
+
+// Task 
+let userpin=74234
+
+i=0
+
+
+while(i<3){
+
+       const userinput=+prompt("Enter Your Pin Code");
+    if(userpin==userinput){
+        alert("Successful Login")
+        break
+
+    }
+    else if(i==2){
+        alert("Account Is Freez")
+    }   
+
+    else{
+        alert("Try Again Once More")
+    }
+    i++
+}
+
+
+
+
+
+
+let userPin=74234
+
+for(let i=0;i<3;i++){
+    const userinput=+prompt("Enter Your Pin Code");
+    if(userPin==userinput){
+        alert("Successful Login")
+        break
+
+    }
+    else if(i==2){
+        alert("Account Is Freez")
+    }   
+
+    else{
+        alert("Try Again Once More")
+    }
+
+}
+
