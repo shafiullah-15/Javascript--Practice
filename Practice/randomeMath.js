ed0880709f6c887c7632171bb103777d00de68cf
SHAFI ULLAH <shafiullah15official@gmail.com>
2025-05-19 09:43:02 +0500

    change

---
+++ Practice/randomeMath.js
@@ -0,0 +1,24 @@
+let roundNo=56.3556;
+let ceilNo=54.525;
+let floorNo=655.82;
+let randomNo=
+
+
+console.log(Math.round(roundNo))
+console.log(Math.floor(floorNo))
+console.log(Math.ceil(ceilNo))
+console.log(Math.random(randomNo))
+
+let randomnumber1=Math.random()*100;
+let randomnumber2=Math.random()*100;
+let randomnumber3=Math.random()*100;
+
+        console.log(Math.round(randomnumber1))
+        console.log(Math.round(randomnumber2))
+        console.log(Math.round(randomnumber3))
+
+let random1=0;
+let random2=0;
+let random3=0
+console.log(`rgb(${Math.round(Math.random(random1)*255)},${Math.round(Math.random(random2)*255)},${Math.round(Math.random(random3)*255)})`)
+
